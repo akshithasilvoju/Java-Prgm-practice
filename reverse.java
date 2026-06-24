2b459671862d205728f4aa19fedfc3108add9e47
akshithasilvoju <164383477+akshithasilvoju@users.noreply.github.com>
2026-06-24 17:43:36 +0530

    Add files via upload
    Reverse Number using Java

---
+++ reverse.java
@@ -0,0 +1,11 @@
+public class reverse {
+    public static void main(String[] args) {
+        int n = 123421,rev=0;
+        while(n!=0){
+            int digit= n%10;
+             rev= rev*10+digit;
+             n=n/10;
+        }
+        System.out.println(rev);
+    }
+}
